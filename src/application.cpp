1364c5526a85bc750917b02390ff1d20bc31ea1b
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2024-01-31 12:31:58 +0100

    Same camera parameters regardless of model loaded

--- src/application.cpp
+++ src/application.cpp
@@ -273,52 +273,20 @@ void application_main(const char *file_name)
     state.context = create_vulkan_context(window);
     SCOPE_EXIT([&] { destroy_vulkan_context(state.context); });
 
-    bool normalize {false};
-    if (std::string(file_name).ends_with("cornell_box.obj"))
-    {
-        const vec3 position {278.0f, 273.0f, -800.0f};
-        const vec3 look_at {278.0f, 273.0f, 0.0f};
-        const auto aspect_ratio = static_cast<float>(render_width) /
-                                  static_cast<float>(render_height);
-        const float focal_length {0.035f};
-        const float sensor_height {0.025f};
-        const auto sensor_width = aspect_ratio * sensor_height;
-        state.camera = create_camera(
-            position, look_at, focal_length, sensor_width, sensor_height);
-    }
-    else if (std::string(file_name).ends_with("dragon.obj"))
-    {
-        const vec3 position {-0.07f, 0.15f, 0.2f};
-        const vec3 look_at {-0.02f, 0.12f, 0.0f};
-        const auto vertical_fov = 45.0f / 180.0f * std::numbers::pi_v<float>;
-        const auto aspect_ratio = static_cast<float>(render_width) /
-                                  static_cast<float>(render_height);
-        const float focal_length {1.0f};
-        const auto sensor_height = 2.0f * std::tan(vertical_fov * 0.5f);
-        const auto sensor_width = aspect_ratio * sensor_height;
-        state.camera = create_camera(
-            position, look_at, focal_length, sensor_width, sensor_height);
-    }
-    else
-    {
-        const vec3 position {2.0f, 1.5f, 2.0f};
-        const vec3 look_at {0.0f, 0.0f, 0.0f};
-        const auto vertical_fov = 45.0f / 180.0f * std::numbers::pi_v<float>;
-        const auto aspect_ratio = static_cast<float>(render_width) /
-                                  static_cast<float>(render_height);
-        const float focal_length {1.0f};
-        const auto sensor_height = 2.0f * std::tan(vertical_fov * 0.5f);
-        const auto sensor_width = aspect_ratio * sensor_height;
-        state.camera = create_camera(
-            position, look_at, focal_length, sensor_width, sensor_height);
-        normalize = true;
-    }
+    const vec3 position {0.0f, 0.0f, -3.5f};
+    const vec3 target {0.0f, 0.0f, 0.0f};
+    const auto vertical_fov = 45.0f / 180.0f * std::numbers::pi_v<float>;
+    const auto aspect_ratio =
+        static_cast<float>(render_width) / static_cast<float>(render_height);
+    const float focal_length {1.0f};
+    const auto sensor_height =
+        2.0f * std::tan(vertical_fov * 0.5f) * focal_length;
+    const auto sensor_width = aspect_ratio * sensor_height;
+    state.camera = create_camera(
+        position, target, focal_length, sensor_width, sensor_height);
 
     Assimp::Importer importer;
-    if (normalize)
-    {
-        importer.SetPropertyBool(AI_CONFIG_PP_PTV_NORMALIZE, true);
-    }
+    importer.SetPropertyBool(AI_CONFIG_PP_PTV_NORMALIZE, true);
 
     const auto *const scene = importer.ReadFile(
         file_name,
