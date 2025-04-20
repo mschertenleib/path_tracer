ce35592de15776a897d6ee5755ee290506cefc0b
Mathieu Schertenleib <schertenleib.mathieu@gmail.com>
2025-04-20 03:10:44 +0200

    Print just the filename component of argv[0] in case of wrong arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "application.hpp"
 
 #include <cstdlib>
+#include <filesystem>
 #include <iostream>
 #include <stdexcept>
 
@@ -10,30 +11,26 @@ int main(int argc, char *argv[])
     {
         if (argc > 2 || (argc == 2 && argv[1][0] == '-'))
         {
-            std::cout << "Usage: " << argv[0] << " [<file>]\n";
+            std::cout << "Usage: "
+                      << std::filesystem::path(argv[0]).filename().string()
+                      << " [<file>]\n";
             return EXIT_FAILURE;
         }
 
-        const char *file_name {};
-        if (argc == 2)
-        {
-            file_name = argv[1];
-        }
-
-        run(file_name);
+        run(argc == 2 ? argv[1] : nullptr);
 
         return EXIT_SUCCESS;
     }
     catch (const std::exception &e)
     {
         std::cout << std::flush;
-        std::cerr << "Exception thrown: " << e.what() << '\n';
+        std::cerr << "Exception thrown: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
     catch (...)
     {
         std::cout << std::flush;
-        std::cerr << "Unknown exception thrown\n";
+        std::cerr << "Unknown exception thrown" << std::endl;
         return EXIT_FAILURE;
     }
 }
